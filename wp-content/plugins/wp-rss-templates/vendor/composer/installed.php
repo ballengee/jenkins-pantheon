0445c298135d5403f1ccdb154b53e68d8843bf32
Matt Ballengee <ballengeem@ecu.edu>
2022-09-23 20:04:02 +0000

    add WP RSS Aggregator plugins and Neve theme

---
+++ wp-content/plugins/wp-rss-templates/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c506c7ea01999c6a2373a5aca619d64283f59f44',
+    'name' => 'wprss/templates',
+  ),
+  'versions' => 
+  array (
+    'wprss/templates' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c506c7ea01999c6a2373a5aca619d64283f59f44',
+    ),
+  ),
+);
